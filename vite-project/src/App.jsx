851bf67fc7e0c607e2558e34597d881cabe389c8
mariomarquesto <mariomarquesto@gmail.com>
2023-08-20 00:05:45 -0300

    segundo commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,23 +1,22 @@
-import { useState } from 'react'
 import ReactAudioPlayer from 'react-audio-player';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-       <h1>HOLAAAAA</h1>
-      </div>
-      <div className="audio-container">
+      <div className="App">
+        <h1>FRANCISCO MARQUESTO</h1>
+
+
         <ReactAudioPlayer
           src="./src/audio/AudioFran.mp3" // Asegúrate de que la ruta sea correcta
           autoPlay
           controls
         />
       </div>
-     
+
     </>
   )
 }
